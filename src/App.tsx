4650979564f9708de2f851cd8d52115b9bda5bde
2ijies11 <94870386+2ijies11@users.noreply.github.com>
2025-09-20 07:15:47 +0000

    サンプル追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,61 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { type NotificationPayload } from 'firebase/messaging';
+import { initializeApp } from "firebase/app";
+import { getToken, getMessaging, onMessage } from "firebase/messaging";
+import { firebaseConfig, vapidKey } from './config';
+import { Snackbar, Alert, AlertTitle } from '@mui/material';
+
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+function requestPermission() {
+  console.log('Requesting permission...');
+  Notification.requestPermission()
+    .then((permission) => {
+      if (permission === 'granted') {
+        console.log('Notification permission granted.');
+      }
+    })
+};
+
 function App() {
   const [count, setCount] = useState(0)
+  const [message, setMessage] = useState<NotificationPayload | undefined>(undefined);
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    getToken(messaging, { vapidKey })
+      .then((currentToken) => {
+        if (currentToken) {
+          console.log('トークンを取得しました。', currentToken);
+        } else {
+          console.log('トークンが利用できません。');
+          requestPermission();
+        }
+      })
+      .catch((err) => {
+        console.log('トークンの取得に失敗しました。', err);
+      });
+  }, []);
+
+  useEffect(() => {
+    const unregisterFunc = onMessage(messaging, (payload) => {
+      console.log('Message received. ', payload);
+      if (payload.notification) {
+        setMessage(payload.notification);
+        setOpen(true);
+      }
+    });
+    return () => unregisterFunc();
+  }, []);
 
   return (
     <>
@@ -28,6 +79,17 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Snackbar
+        open={open}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <Alert onClose={handleClose} severity="info" sx={{ width: '100%' }}>
+          <AlertTitle>{message?.title}</AlertTitle>
+          {message?.body}
+        </Alert>
+      </Snackbar >
     </>
   )
 }
---
+++ src/config.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCO0leHI_zVN1v3goC082WlHjLKZqokUGo",
+  authDomain: "web-push-9092f.firebaseapp.com",
+  databaseURL: 'https://web-push-9092f-default-rtdb.firebaseio.com',
+  projectId: "web-push-9092f",
+  storageBucket: "web-push-9092f.firebasestorage.app",
+  messagingSenderId: "139283173258",
+  appId: "1:139283173258:web:e587d24efebb93a9518273"
+};
+
+export const vapidKey = 'BA6W3CGTBY1jquHmverqNnGjqa3LD322qCU6qipyzFT8CtHQZhlPoZY15WlZUVnSF_xEVdaUyX_0Ni407pnJ8Ps';
